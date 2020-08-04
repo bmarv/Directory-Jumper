10ac966943cf7227086a2ec2ca1b3773efd4fcb0
Marvin Mac <beesemarvin@gmail.com>
2020-08-04 16:29:10 +0200

    Comments added

--- DirectoryJumper.java
+++ DirectoryJumper.java
@@ -1,16 +1,27 @@
 class DirectoryJumper{
     DirectoryJumper(){
     }
+    /**
+     * Main Method that invokes the runProgram-Method after checking that the 
+     * size of the passed argument is exactly one
+     * @param args
+     */
     public static void main(String args[]){
         // check for correct number of arguments
         if(args.length!=1)
-            System.out.println("Please insert only one directory");
+            System.out.println("Please insert exactly one directory-name");
         else{
             String targetDirectory = runProgram(args[0]);
             System.out.println(targetDirectory);
         }
     }
 
+    /**
+     * Method that invokes the DirectoryOperation-Class, which includes the 
+     * program logic
+     * @param targetDir
+     * @return Path of the user-defined target-directory
+     */
     public static String runProgram(String targetDir){
         DirectoryOperations OperationHandler = new DirectoryOperations(targetDir);
         return OperationHandler.targetPathString;
--- DirectoryOperations.java
+++ DirectoryOperations.java
@@ -11,16 +11,33 @@ public class DirectoryOperations {
         this.targetPathString = routeToTarget(currentDirectory, targetDir, superDirectories);
     }
 
+    /**
+     * gets the current directory of the JVM
+     * @return Path of the current directory
+     */
     Path getDir(){
         Path currentPath = Paths.get("").toAbsolutePath();
         return currentPath;
     }
 
+    /**
+     * splits the directories within the current path
+     * @param currentPath
+     * @return String-Array of directory names
+     */
     String[] getSuperDirectories(Path currentPath){
         String superDirs[] = currentPath.toString().split("/");      
         return superDirs;
     }
 
+    /**
+     * searches and matches the user-defined directory with an element
+     * of the String-Array with directory names
+     * @param currentDir
+     * @param target
+     * @param superDirs
+     * @return String with Path to the target-directory in Unix-Format
+     */
     String routeToTarget(Path currentDir, String target, String superDirs[]){
         String targetDirString = null;
         for (String superDir: superDirs){
